7bfad019ee80a104f6e5a335f570d74e9159a841
matej352 <matej.durin1@gmail.com>
2022-12-30 11:20:11 +0100

    caching works

--- app.js
+++ app.js
@@ -36,6 +36,21 @@ app.get('/menu', async function (req, res) {
 });
 
 
+app.get('/about-us', async function (req, res) {
+	res.render('about-us');
+});
+
+
+
+app.get('/not-found', async function (req, res) {
+	res.render('not-found');
+});
+
+app.get('*', function(req, res){
+	res.status(404).render('not-found')
+  });
+
+
 if (externalUrl) {
 	const hostname = '127.0.0.1';
 	app.listen(PORT, hostname, () => {
--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,76 @@
-self.addEventListener('install', event => {
-    console.log('Service worker installing…');
+const filesToCache = [
+    "home",
+    "menu",
+    "not-found",
+    "about-us",
+    "manifest.json",
+];
+
+const staticCacheName = "static-cache-my-v1";
+
+self.addEventListener("install", (event) => {
+    console.log("Attempting to install service worker and cache static assets");
+    event.waitUntil(
+        caches.open(staticCacheName).then((cache) => {
+            return cache.addAll(filesToCache);
+        })
+    );
 });
 
-self.addEventListener('activate', event => {
-    console.log('Service worker installing…');
+self.addEventListener("activate", (event) => {
+    console.log(
+        "*************************************************************************************"
+    );
+    console.log(
+        "******************   Activating new service worker... *******************************"
+    );
+    console.log(
+        "*************************************************************************************"
+    );
+
+    const cacheWhitelist = [staticCacheName];
+    // Ovako možemo obrisati sve ostale cacheve koji nisu naš
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
 
-self.addEventListener('fetch', () => console.log("fetch"));
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then((response) => {
+                if (response) {
+                    console.log("Found " + event.request.url + " in cache!");
+                    return response;
+                }
+                console.log(
+                    "----------------->> Network request for ",
+                    event.request.url
+                );
+                return fetch(event.request).then((response) => {
+                    console.log("response.status = " + response.status);
+                    if (response.status === 404) {
+                        return caches.match("not-found");
+                    }
+                    return caches.open(staticCacheName).then((cache) => {
+                        console.log(">>> Caching: " + event.request.url);
+                        cache.put(event.request.url, response.clone());
+                        return response;
+                    });
+                });
+            })
+            .catch((error) => {
+                console.log("Error", event.request.url, error);
+                
+            })
+    );
+});
