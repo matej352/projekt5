57ebc1308152ed71fa907539b23367de4bdbf5a1
matej352 <matej.durin1@gmail.com>
2023-01-01 20:17:56 +0100

    add native api and background sync

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 const pizzas = require('./database/pizzas.js')
 
+const multer = require("multer");
+const fse = require('fs-extra');
 
 const app = express();
 
@@ -28,8 +30,6 @@ app.get('/home', async function (req, res) {
 
 app.get('/menu', async function (req, res) {
 
-	//console.log(pizzas.pizzas)
-
 	res.render('menu', {
 		pizzas: pizzas.pizzas,
 	});
@@ -45,6 +45,65 @@ app.get('/about-us', async function (req, res) {
 app.get('/not-found', async function (req, res) {
 	res.render('not-found');
 });
+
+
+
+// potrebno za VER05+
+const UPLOAD_PATH = path.join(__dirname, "public", "uploads");
+var uploadSnaps = multer({
+    storage:  multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, UPLOAD_PATH);
+        },
+        filename: function (req, file, cb) {
+            let fn = file.originalname.replaceAll(":", "-");
+            cb(null, fn);
+        },
+    })
+}).single("image");
+
+
+app.post("/saveSnap",  function (req, res) {
+    uploadSnaps(req, res, async function(err) {
+        if (err) {
+            console.log(err);
+            res.json({
+                success: false,
+                error: {
+                    message: 'Upload failed:: ' + JSON.stringify(err)
+                }
+            });
+        } else {
+            console.log(req.body);
+            res.json({ success: true, id: req.body.id });
+            //if (VERSION==="06") await sendPushNotifications(req.body.title);
+        }
+    });
+});
+
+
+app.get("/snaps", function (req, res) {
+    let files = fse.readdirSync(UPLOAD_PATH);
+    //files = files.reverse().slice(0, 10);
+    console.log("In", UPLOAD_PATH, "there are", files);
+    res.json({
+        files
+    });
+});
+// /potrebno za VER05+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.get('*', function(req, res){
 	res.status(404).render('not-found')
--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,9 @@
+function entries(t = r()) { return t("readonly", (n => { if (n.getAll && n.getAllKeys) return Promise.all([e(n.getAllKeys()), e(n.getAll())]).then((([e, t]) => e.map(((e, n) => [e, t[n]])))); const r = []; return t("readonly", (e => d(e, (e => r.push([e.key, e.value]))).then((() => r)))) })) }
+function del(t, n = r()) { return n("readwrite", (n => (n.delete(t), e(n.transaction)))) }
+function r(){return n||(n=t("keyval-store","keyval")),n}
+function t(t,n){const r=indexedDB.open(t);r.onupgradeneeded=()=>r.result.createObjectStore(n);const o=e(r);return(e,t)=>o.then((r=>t(r.transaction(n,e).objectStore(n))))}let n;
+function e(e){return new Promise(((t,n)=>{e.oncomplete=e.onsuccess=()=>t(e.result),e.onabort=e.onerror=()=>n(e.error)}))}
+
 const filesToCache = [
     "home",
     "menu",
@@ -44,6 +50,7 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
+   
     event.respondWith(
         caches
             .match(event.request)
@@ -70,7 +77,48 @@ self.addEventListener("fetch", (event) => {
             })
             .catch((error) => {
                 console.log("Error", event.request.url, error);
-                
+
             })
     );
 });
+
+self.addEventListener('sync', function (event) {
+    console.log('Background sync!', event);
+    if (event.tag === 'sync-snaps') {
+        event.waitUntil(
+            syncSnaps()
+        );
+    }
+});
+
+let syncSnaps = async function () {
+    entries()
+        .then((entries) => {
+            entries.forEach((entry) => {
+                let snap = entry[1]; //  Each entry is an array of [key, value].
+                let formData = new FormData();
+                formData.append('id', snap.id);
+                formData.append('ts', snap.ts);
+                formData.append('title', snap.title);
+                formData.append('image', snap.image, snap.id + '.png');
+                fetch('/saveSnap', {
+                    method: 'POST',
+                    body: formData
+                })
+                    .then(function (res) {
+                        if (res.ok) {
+                            res.json()
+                                .then(function (data) {
+                                    console.log("Deleting from idb:", data.id);
+                                    del(data.id);
+                                });
+                        } else {
+                            console.log(res);
+                        }
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            })
+        });
+}
